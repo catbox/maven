0de6f49db3b6f90cf1b294c446ee03ba199d0bf0
cocolekat <cocolekat@gmail.com>
2014-06-25 18:52:27 -0400

    test11
    Signed-off-by: cocolekat <cocolekat@gmail.com>

--- Test1/src/com/pkw/MainTest1.java
+++ Test1/src/com/pkw/MainTest1.java
@@ -6,7 +6,7 @@ public class MainTest1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Test1. How are you test1?");
+		System.out.println("Hello Test1. How are you test11?");
 
 	}
 
