3cf439269dec3b3e54afd928e240ceb7c42dfa69
cocolekat <cocolekat@gmail.com>
2014-06-26 17:57:35 -0400

    Maven App
    Signed-off-by: cocolekat <cocolekat@gmail.com>

---
+++ HelloMaven/src/main/java/com/hello/maven/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.hello.maven;
+
+public class HelloMaven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+
+	}
+
+}
---
+++ HelloMaven/src/test/java/com/hello/maven/test/HelloMavenTest.java
@@ -0,0 +1,12 @@
+package com.hello.maven.test;
+
+import org.junit.Test;
+
+public class HelloMavenTest {
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
